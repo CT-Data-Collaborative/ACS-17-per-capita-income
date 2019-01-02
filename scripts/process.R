b42c7c11696f2d1563de00938b0bd10d599b0872
Zorawar Moolenaar <zorawar87@gmail.com>
2019-01-02 10:00:57 -0500

    update

--- scripts/process.R
+++ scripts/process.R
@@ -5,12 +5,12 @@ source("./scripts/utils.R")
 sub_folders <- list.files()
 raw_location <- grep("raw", sub_folders, value=T)
 path_to_raw <- (paste0(getwd(), "/", raw_location))
-raw_data <- dir(path_to_raw, recursive=T, pattern = "B19013_with_ann", full.names = TRUE)
+raw_data <- dir(path_to_raw, recursive=T, pattern = "B19301_with_ann", full.names = TRUE)
 
 # extract information and write to CSV file
-ct.income<- getCTHouseholdIncome(raw_data)
+ct.income<- getCTPerCapitaIncome(raw_data)
 counties <- getGatheredCountyData(ct.income)
 towns    <- getGatheredTownData(ct.income)
 
-WriteDFToTable(counties, "median-household-income-by-county-ACS_2017.csv")
-WriteDFToTable(towns, "median-household-income-by-town-ACS_2017.csv")
+WriteDFToTable(counties, "per-capita-income-by-county-ACS_2017.csv")
+WriteDFToTable(towns, "per-capita-income-by-town-ACS_2017.csv")
